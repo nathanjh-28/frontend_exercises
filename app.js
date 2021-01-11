1ae540cfaf150a16a5f7e9dda4c95305e2963110
nathanjh-28 <nathan@nathanjharris.com>
2021-01-11 08:54:33 -0700

    links added to index.js and md

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const fp = []; const ap = [];
 
 // TEMPLATE - (  [  DATE  ,  DIRNAME  ,  TITLE  ]  )
 
+fp.push(['Jan 11, 2021','random_choice_picker','Random Choice Picker']);
 fp.push(['Jan 9, 2021','FAQ_collapse','FAQ Collapse']);
 fp.push(['Jan 8, 2021','event_key_codes','Event Key Codes']);
 fp.push(['Jan 7, 2021','dad_jokes','Dad Jokes']);
