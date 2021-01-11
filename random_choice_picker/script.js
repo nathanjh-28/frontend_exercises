3326573adf3166d944bfc10ae4d26106fde9a8eb
nathanjh-28 <nathan@nathanjharris.com>
2021-01-11 08:52:14 -0700

    random choice picker complete

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -5,6 +5,12 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e)=>{
     createTags(e.target.value)
+    if(e.key=== 'Enter'){
+        setTimeout(() => {
+            e.target.value = ''
+        },10)
+        randomSelect()
+    }
 })
 
 function createTags(input){
@@ -18,5 +24,36 @@ function createTags(input){
         tagEl.innerText = tag
         tagsEl.appendChild(tagEl)
     })
-    console.log(tags)
+}
+
+function randomSelect(){
+    const times = 30
+    const interval = setInterval(()=>{
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(()=>{
+            unhighlightTag(randomTag)
+        }, 100);
+    }, 100);
+    setTimeout(()=>{
+        clearInterval(interval)
+        setTimeout(()=>{
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+        },100)
+    },times * 100)
+}
+
+function pickRandomTag(){
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+    tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag){
+    tag.classList.remove('highlight')
 }
