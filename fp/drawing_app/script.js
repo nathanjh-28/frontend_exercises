b36e100c1224721f1aa2e7dc53ce13742731ad11
nathanjh-28 <nathan@nathanjharris.com>
2021-01-20 08:39:30 -0700

    drawing app complete

--- fp/drawing_app/script.js
+++ fp/drawing_app/script.js
@@ -1,9 +1,16 @@
 // Canvas API
 
 const canvas = document.getElementById('canvas')
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorEl = document.getElementById('color')
+const clearEl = document.getElementById('clear')
+
 const ctx = canvas.getContext('2d')
 
-let size = 20
+
+let size = 10
 isPressed = false
 let color = 'black'
 let x; let y;
@@ -50,7 +57,29 @@ function drawLine(x1,y1,x2,y2){
 
 }
 
+function updateSizeOnScreen(){
+    sizeEl.innerText = size
+}
 
-// drawCircle(100,200)
-// drawLine(300, 300, 300, 500)
 
+
+// Toolbox 
+
+increaseBtn.addEventListener('click',()=>{
+    size +=5
+    if(size > 50){
+        size = 50
+    }
+    updateSizeOnScreen()
+})
+decreaseBtn.addEventListener('click',()=>{
+    size -=5
+    if(size < 5){
+        size = 5
+    }
+    updateSizeOnScreen()
+})
+
+colorEl.addEventListener('change',(e)=> color = e.target.value)
+
+clearEl.addEventListener('click',()=>ctx.clearRect(0,0,canvas.width,canvas.height))
