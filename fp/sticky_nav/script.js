2cb7947d43d0c350ab05cfd022e968fdbeae70fd
nathanjh-28 <nathan@nathanjharris.com>
2021-01-23 09:12:32 -0700

    stick nav pjt complete

--- fp/sticky_nav/script.js
+++ fp/sticky_nav/script.js
@@ -1,1 +1,10 @@
-console.log('starter project engaged')
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav(){
+    if(window.scrollY > nav.offsetHeight+150){
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
