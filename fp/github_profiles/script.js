3f168a40c39050c0b09017bd9a808a83685b6b23
nathanjh-28 <nathan@nathanjharris.com>
2021-01-26 09:41:21 -0700

    axios get requests complete

--- fp/github_profiles/script.js
+++ fp/github_profiles/script.js
@@ -1,1 +1,29 @@
-console.log('starter project engaged')
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+
+// function getUser(username){
+//     axios(APIURL + username)
+//         .then(res=> console.log(res.data.name))
+//         .catch(err => console.log(err))
+// }
+
+async function getUser(username){
+    try {
+        const { data } = await axios(APIURL + username)
+        console.log(data)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const user = search.value
+    if(user){
+        getUser(user)
+        search.value = ''
+    }
+})
