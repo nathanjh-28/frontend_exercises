5245d2f283e48e8c7aaaec5a8afffd5e6a7d19b1
nathanjh-28 <nathan@nathanjharris.com>
2021-02-05 09:20:00 -0700

    root index updated

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+
+// Playing with local JSON file
+
+const getData = async () =>{
+    const res = await fetch('data.json')
+    const data = await res.json()
+    return data
+} 
+
+const data = getData()
+.then((data)=>{
+    console.log(data)
+})
+
+
 const pjtSection = document.querySelector('#project50')
 const addPrjts = document.querySelector('#add-Prjts')
 const fp = []; const ap = [];
@@ -6,6 +21,7 @@ const fp = []; const ap = [];
 
 // TEMPLATE - (  [  DATE  ,  DIRNAME  ,  TITLE  ]  )
 
+fp.push(['Feb 5, 2021', 'pokedex', 'Pokedex']);
 fp.push(['Feb 4, 2021', 'hoverboard', 'Hoverboard']);
 fp.push(['Feb 3, 2021', 'image_carousel', 'Image Carousel']);
 fp.push(['Feb 2, 2021', 'animated_countdown', 'Animated Countdown']);
