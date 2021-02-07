7e0b8b2ed2b510fb7df85eb2075c00cbd60c3ac1
nathanjh-28 <nathan@nathanjharris.com>
2021-02-07 08:45:22 -0700

    password bg picture blur pjt complete

--- fp/pass_strength_bg/script.js
+++ fp/pass_strength_bg/script.js
@@ -1,1 +1,24 @@
-console.log('starter project engaged')
+const password = document.getElementById('password')
+const background = document.getElementById('background')
+
+
+// My Solution
+// let blurVal = 20
+// password.addEventListener('keydown', (e)=>{
+//     eHandle(e)
+// })
+
+// function eHandle (e){
+//     if(e.key === 'Backspace')blurVal++
+//     else {blurVal = blurVal - password.value.length+1}
+//     background.style.filter = `blur(${blurVal}px)`
+// }
+
+password.addEventListener('input',(e)=>{
+    const input = e.target.value 
+    length = input.length
+    const blurValue = 20-length * 2
+    background.style.filter = `blur(${blurValue}px)`
+
+    console.log(20-length * 2)
+})
