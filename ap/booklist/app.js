c298eef5eefeefae6910e2a64fbe4ab481411e33
nathanjh-28 <nathan@nathanjharris.com>
2021-02-10 10:40:17 -0700

    es5 version complete

--- ap/booklist/app.js
+++ ap/booklist/app.js
@@ -41,12 +41,16 @@ UI.prototype.showAlert = function(message,className){
 
 }
 
+UI.prototype.deleteBook = function(target){
+    if(target.className === 'delete'){
+        target.parentElement.parentElement.remove();
+    }
+}
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = ''
     document.getElementById('author').value = ''
     document.getElementById('isbn').value = ''
 }
-
 
 
 // Event Listeners
@@ -77,4 +81,17 @@ document.getElementById('book-form').addEventListener('submit',(e)=>{
 
 
     e.preventDefault();
+})
+
+// Event listener for delete
+
+document.getElementById('book-list').addEventListener('click',(e)=>{
+    
+    const ui = new UI()
+    ui.deleteBook(e.target)
+    ui.showAlert('book deleted','success')
+    
+    
+    e.preventDefault()
+
 })
