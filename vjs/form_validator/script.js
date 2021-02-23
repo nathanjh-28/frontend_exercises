12e9d8fbe13bf0b0b1ffee04f5a2de7f48b42c8c
nathanjh-28 <nathan@nathanjharris.com>
2021-02-23 10:59:22 -0700

    form validator pjt complete

--- vjs/form_validator/script.js
+++ vjs/form_validator/script.js
@@ -16,9 +16,15 @@ function showSuccess(input) {
     formControl.className = 'form-control success'
 }
 
-function isValidEmail(email) {
+
+
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input)
+    } else {
+        showErr(input, 'Email is not valid')
+    }
 }
 
 function checkRequired(inputArr) {
@@ -29,21 +35,40 @@ function checkRequired(inputArr) {
             showSuccess(input);
         }
     });
+}
 
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showErr(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showErr(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
 }
 
 function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showErr(input2, 'Passwords do not match');
+    }
+}
+
 // Event Listeners
 form.addEventListener('submit', function (e) {
     e.preventDefault()
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15)
+    checkLength(password, 6, 25)
+    checkEmail(email)
+    checkPasswordsMatch(password, password2)
 
 })
 
-// ====== First version before Refactor
+// ====== First version before Refactor ======
 
 // form.addEventListener('submit', function (e) {
 //     e.preventDefault()
@@ -70,3 +95,8 @@ form.addEventListener('submit', function (e) {
 //         showSuccess(password2);
 //     }
 // })
+
+//function isValidEmail(email) {
+    //     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     return re.test(String(email).toLowerCase());
+    // }
