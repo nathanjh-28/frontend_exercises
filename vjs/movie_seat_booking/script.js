3f64e16e540c966a73f106b52ac78a817595dbd6
nathanjh-28 <nathan@nathanjharris.com>
2021-02-24 09:42:41 -0700

    movie theater app complete no local storage

--- vjs/movie_seat_booking/script.js
+++ vjs/movie_seat_booking/script.js
@@ -1,1 +1,30 @@
-console.log('starter project engaged')
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+
+let ticketPrice = +movieSelect.value
+
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    count.innerText = selectedSeats.length
+    total.innerText = selectedSeats.length * ticketPrice
+}
+
+// Movie Select Event
+movieSelect.addEventListener('change', e => {
+    ticketPrice = +e.target.value
+    updateSelectedCount()
+
+})
+
+// Seat click event
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected')
+        updateSelectedCount()
+    }
+})
+
