e47760c6d467516579132487e5cac2983fc8e82f
nathanjh-28 <nathan@nathanjharris.com>
2021-02-24 10:41:35 -0700

    movie app with adding data to local storage

--- vjs/movie_seat_booking/script.js
+++ vjs/movie_seat_booking/script.js
@@ -6,17 +6,31 @@ const movieSelect = document.getElementById('movie')
 
 let ticketPrice = +movieSelect.value
 
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex)
+    localStorage.setItem('selectedMoviePrice', moviePrice)
+}
 
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+    // Copy selected seats in to an array
+    // Map through array
+    // return new array of indexes
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     count.innerText = selectedSeats.length
     total.innerText = selectedSeats.length * ticketPrice
 }
 
 // Movie Select Event
 movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value
-    updateSelectedCount()
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value)
+    updateSelectedCount();
 
 })
 
