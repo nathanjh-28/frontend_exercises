caf28ca29b69101b0acddd7a0aeee494e6640d7a
nathanjh-28 <nathan@nathanjharris.com>
2021-02-25 10:57:04 -0700

    moved one project outside repo for space reasonings

--- ap/travel_video_page/script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.toggle')
-const showcase = document.querySelector('.showcase')
-
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('active')
-    showcase.classList.toggle('active')
-
-})
