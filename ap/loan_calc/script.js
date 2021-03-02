cc7521d3484ff3f93ef45c49b203a0c587188aa7
nathanjh-28 <nathan@nathanjharris.com>
2021-03-02 09:25:09 -0700

    loan calc complete

--- ap/loan_calc/script.js
+++ ap/loan_calc/script.js
@@ -1,9 +1,20 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults)
+document.getElementById('loan-form').addEventListener('submit', (e) => {
+
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault()
+})
 
 // Calc Function
-function calculateResults(e) {
-    e.preventDefault();
+function calculateResults() {
+
 
     // ui variables
     const amount = document.getElementById('amount');
@@ -26,7 +37,9 @@ function calculateResults(e) {
     if (isFinite(monthly)) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please Check Your Numbers')
     }
@@ -35,6 +48,8 @@ function calculateResults(e) {
 }
 
 function showError(error) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
     // create div
     const errorDiv = document.createElement('div');
     const card = document.querySelector('.card');
