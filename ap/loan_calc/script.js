eab596dce8e9a9529620f0be12954d880296141a
nathanjh-28 <nathan@nathanjharris.com>
2021-03-02 09:17:16 -0700

    added error messaging for bad inputs

--- ap/loan_calc/script.js
+++ ap/loan_calc/script.js
@@ -28,8 +28,27 @@ function calculateResults(e) {
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
     } else {
-        console.log('Please Check your numbers')
+        showError('Please Check Your Numbers')
     }
 
     console.log(principal)
+}
+
+function showError(error) {
+    // create div
+    const errorDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error))
+
+    card.insertBefore(errorDiv, heading);
+
+    // clear error after 3sec
+
+    setTimeout(clearError, 3000);
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
 }
