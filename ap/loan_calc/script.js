da8314554e9f02cbe65ff4e9aa3c41606e67cba8
nathanjh-28 <nathan@nathanjharris.com>
2021-03-02 09:10:41 -0700

    calculations js complete

---
+++ ap/loan_calc/script.js
@@ -0,0 +1,35 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults)
+
+// Calc Function
+function calculateResults(e) {
+    e.preventDefault();
+
+    // ui variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+
+    // Get figures
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute Monthly Payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+    } else {
+        console.log('Please Check your numbers')
+    }
+
+    console.log(principal)
+}
