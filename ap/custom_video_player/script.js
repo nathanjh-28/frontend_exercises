bfb0c7b3be977c6ee2847df97136703a45f40cd5
nathanjh-28 <nathan@nathanjharris.com>
2021-03-03 11:16:35 -0700

    play pause and stop functionality added

--- ap/custom_video_player/script.js
+++ ap/custom_video_player/script.js
@@ -1,1 +1,49 @@
-console.log('starter project engaged')
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Play and Pause Video
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// update play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+// update progress and timestamp
+function updateProgress() {
+    return true;
+}
+
+// Set Video time to progress
+function setVideoProgress() {
+    return true;
+}
+
+// Stop Video
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+
+// Event Listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
