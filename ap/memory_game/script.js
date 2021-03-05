1d3e21349c785a8ce25df7455b002c7222c56cb3
nathanjh-28 <nathan@nathanjharris.com>
2021-03-04 17:18:27 -0700

    game created but need win state

--- ap/memory_game/script.js
+++ ap/memory_game/script.js
@@ -51,21 +51,47 @@ const cardArray = [
 ];
 
 const grid = document.querySelector('.grid');
-const cardsChosen = []
-const cardsChosenId = []
+let cardsChosen = []
+let cardsChosenId = []
+const cardsWon = []
 
 function createBoard() {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img');
         card.setAttribute('src', 'img/card.jpg')
         card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard)
         grid.appendChild(card)
     }
 }
 
+function checkForMatch() {
+    const cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('you found a match');
+        cards[optionOneId].setAttribute('src', 'img/white.png');
+        cards[optionTwoId].setAttribute('src', 'img/white.png');
+        cardsWon.push(cardsChosen);
+    } else {
+        cards[optionOneId].setAttribute('src', 'img/card.jpg');
+        cards[optionTwoId].setAttribute('src', 'img/card.jpg');
+        alert('Sorry try again')
+    }
+    cardsChosen = []
+    cardsChosenId = []
+}
+
 function flipCard() {
     var cardId = this.getAttribute('data-id');
-    cardsChosen.push(cardArray[cardId].name)
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+
 }
 
 
