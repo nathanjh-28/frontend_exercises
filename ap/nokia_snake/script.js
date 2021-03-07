34e36138a74670c7ec9083db84793b209b67f6de
nathanjh-28 <nathan@nathanjharris.com>
2021-03-07 15:31:46 -0700

    app state started

--- ap/nokia_snake/script.js
+++ ap/nokia_snake/script.js
@@ -1,1 +1,15 @@
-console.log('starter project engaged')
+
+const squares = document.querySelectorAll('.grid div');
+const scoreDisplay = document.querySelector('span');
+const startBtn = document.querySelector('.start');
+
+const width = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let score = 0;
+let speed = 0.9;
+let intervalTime = 0;
+let interval = 0;
+
