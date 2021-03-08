3747b4845f00184498e0977358ba1b642d5083e5
nathanjh-28 <nathan@nathanjharris.com>
2021-03-07 18:20:04 -0700

    start game function created

--- ap/nokia_snake/script.js
+++ ap/nokia_snake/script.js
@@ -13,6 +13,24 @@ let speed = 0.9;
 let intervalTime = 0;
 let interval = 0;
 
+//start game
+
+function startGame() {
+    currentSnake.forEach(index => squares[index].classList.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(interval);
+    score = 0;
+    direction = 1;
+    scoreDisplay.innerText = score;
+    intervalTime = 1000;
+    currentSnake = [2, 1, 0];
+    currentIndex = 0;
+    currentSnake.forEach(index => squares[index].classList.add('snake'));
+    interval = setInterval(moveOutcomes, intervalTime);
+}
+
+startBtn.addEventListener('click', startGame)
+
 function control(e) {
     squares[currentIndex].classList.remove('snake');
     // console.log(e)
