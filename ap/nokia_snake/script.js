ebf1bcc5503feffa134337c9f4e0968cabe49d90
nathanjh-28 <nathan@nathanjharris.com>
2021-03-07 17:56:13 -0700

    snake keyboard controls working

--- ap/nokia_snake/script.js
+++ ap/nokia_snake/script.js
@@ -13,3 +13,35 @@ let speed = 0.9;
 let intervalTime = 0;
 let interval = 0;
 
+function control(e) {
+    squares[currentIndex].classList.remove('snake');
+    if (e.keycode === 39) {
+        direction = 1; // right arrow
+        console.log('move right')
+    } else if (e.keycode === 38) {
+        direction = -width; // up arrow
+        console.log('move up')
+    } else if (e.keycode === 37) {
+        direction = -1; // left arrow
+        console.log('move left')
+    } else if (e.keycode === 40) {
+        direction = +width; // down arrow
+        console.log('move down')
+    }
+}
+document.addEventListener('keyup', (e) => {
+    squares[currentIndex].classList.remove('snake');
+    if (e.keyCode === 39) {
+        direction = 1; // right arrow
+        console.log('move right')
+    } else if (e.keyCode === 38) {
+        direction = -width; // up arrow
+        console.log('move up')
+    } else if (e.keyCode === 37) {
+        direction = -1; // left arrow
+        console.log('move left')
+    } else if (e.keyCode === 40) {
+        direction = +width; // down arrow
+        console.log('move down')
+    }
+})
