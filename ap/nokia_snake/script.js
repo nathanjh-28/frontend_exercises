3944c81a68ab703909695c17e3c836a0a1403bf7
nathanjh-28 <nathan@nathanjharris.com>
2021-03-07 20:13:55 -0700

    stopping for today on the snake game, almost there

--- ap/nokia_snake/script.js
+++ ap/nokia_snake/script.js
@@ -20,6 +20,7 @@ function startGame() {
     squares[appleIndex].classList.remove('apple');
     clearInterval(interval);
     score = 0;
+    randomApple()
     direction = 1;
     scoreDisplay.innerText = score;
     intervalTime = 1000;
@@ -29,7 +30,44 @@ function startGame() {
     interval = setInterval(moveOutcomes, intervalTime);
 }
 
+function moveOutcomes() {
+    if (
+        //if snake hits bottom
+        (currentSnake[0] + width >= (width * width) && direction === width) ||
+        (currentSnake[0] % width === width - 1 && direction === 1) ||
+        (currentSnake[0] % width === 0 && direction === -1) ||
+        (currentSnake[0] - width < 0 && direction === -width) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    ) {
+        return clearInterval(interval);
+    }
+    const tail = currentSnake.pop();
+    squares[tail].classList.remove('snake');
+
+    currentSnake.unshift(currentSnake[0] + direction)
+
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        squares[currentSnake[0]].classList.remove('apple');
+        squares[tail].classList.add('snake')
+        currentSnake.push(tail)
+        randomApple()
+        score++;
+        scoreDisplay.textContent = score;
+        clearInterval(interval);
+        intervalTime = intervalTime * speed;
+        interval = setInterval(moveOutcomes, intervalTime)
+    }
+    squares[currentSnake[0]].classList.add('snake');
+}
+
 startBtn.addEventListener('click', startGame)
+
+function randomApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+}
 
 function control(e) {
     squares[currentIndex].classList.remove('snake');
