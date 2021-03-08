aff44ec0e070023c5dd20051c14c757610da1eb6
nathanjh-28 <nathan@nathanjharris.com>
2021-03-08 08:58:11 -0700

    game code started and player movement capabilities added

--- ap/space_invaders/script.js
+++ ap/space_invaders/script.js
@@ -7,3 +7,43 @@ while (i < 225) {
     i++
 }
 
+/* --------------------------------------------- */
+
+const squares = grid.querySelectorAll('div');
+const resultDisplay = document.getElementById('result');
+let width = 15;
+let currentShooterIndex = 202;
+let currentInvaderIndex = 0;
+let alienInvadersTakenDown = [];
+let result = 0;
+let direction = 1;
+let invaderId;
+
+
+const alienInvaders = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+]
+
+alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'));
+
+squares[currentShooterIndex].classList.add('shooter');
+
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter');
+    switch (e.keyCode) {
+        case 37:
+            if (currentShooterIndex % width !== 0) currentShooterIndex--;
+            break
+        case 39:
+            if (currentShooterIndex % 15 < width - 1) currentShooterIndex++;
+            break
+    }
+    squares[currentShooterIndex].classList.add('shooter');
+}
+document.addEventListener('keydown', moveShooter)
+
+
+// console.log(squares)
