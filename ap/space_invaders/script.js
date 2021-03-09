b78d6d7b4cc75fa4eb7ad94fdd99861bd9c2a682
nathanjh-28 <nathan@nathanjharris.com>
2021-03-09 08:25:09 -0700

    invaders move on set interval and shooter can move by arrow keys

--- ap/space_invaders/script.js
+++ ap/space_invaders/script.js
@@ -1,10 +1,10 @@
 const grid = document.querySelector('.grid');
 
-let i = 1;
-while (i < 225) {
+let newDiv = 1;
+while (newDiv < 225) {
     let div = document.createElement('div');
     grid.appendChild(div)
-    i++
+    newDiv++
 }
 
 /* --------------------------------------------- */
@@ -46,4 +46,37 @@ function moveShooter(e) {
 document.addEventListener('keydown', moveShooter)
 
 
-// console.log(squares)
+function moveInvaders() {
+    const leftEdge = alienInvaders[0] % width === 0;
+    const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1;
+    if ((leftEdge && direction === -1) || (rightEdge && direction === 1)) {
+        direction = 15;
+    } else if (direction === width) {
+        if (leftEdge) direction = 1;
+        else direction = -1;
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+        squares[alienInvaders[i]].classList.remove('invader');
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+        alienInvaders[i] += direction;
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+
+    //decide if game is over
+    if (squares[currentShooterIndex].classList.contains('invader', 'shooter')) {
+        resultDisplay.textContent = 'Game Over';
+        squares[currentShooterIndex].classList.add('boom');
+        clearInterval(invaderId)
+    }
+    for (let i = 0; i <= alienInvaders.length - 1; i++) {
+        if (alienInvaders[i] > (squares.length - (width - 1))) {
+            resultDisplay.textContent = 'Game Over';
+            clearInterval(invaderId)
+        }
+    }
+}
+invaderId = setInterval(moveInvaders, 500);
+
