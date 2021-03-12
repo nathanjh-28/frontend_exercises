10b95fbaf4f503c757f598d588b48912a15c2680
nathanjh-28 <nathan@nathanjharris.com>
2021-03-12 11:37:02 -0700

    refactored for app state

--- ap/css_clock/script.js
+++ ap/css_clock/script.js
@@ -1,20 +1,26 @@
+
+//App State
+const secondHand = $('.second-hand')
+const minHand = $('.min-hand');
+const hourHand = $('.hour-hand');
+
+// Clock Function
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    //console.log(seconds);
     const secondsDeg = ((seconds / 60) * 360) + 90;
-    $('.second-hand').css('transform', `rotate(${secondsDeg}deg)`)
+    secondHand.css('transform', `rotate(${secondsDeg}deg)`)
 
     const mins = now.getMinutes();
     const minsDeg = ((mins / 60) * 360) + 90;
-    $('.min-hand').css('transform', `rotate(${minsDeg}deg)`);
+    minHand.css('transform', `rotate(${minsDeg}deg)`);
 
     let hours = now.getHours();
     if (hours > 12) hours -= 12
     const hoursDeg = ((hours / 12) * 360) + 90;
-    $('.hour-hand').css('transform', `rotate(${hoursDeg}deg)`)
+    hourHand.css('transform', `rotate(${hoursDeg}deg)`)
 
 }
 
-
+// Start Clock
 setInterval(setDate, 1000)
