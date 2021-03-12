b643fc0d4f798135c221b4db912dc52aa685a682
nathanjh-28 <nathan@nathanjharris.com>
2021-03-12 11:38:57 -0700

    css clock complete

--- ap/css_clock/script.js
+++ ap/css_clock/script.js
@@ -7,20 +7,53 @@ const hourHand = $('.hour-hand');
 // Clock Function
 function setDate() {
     const now = new Date();
-    const seconds = now.getSeconds();
+
+    //_________________ SECONDS
+
+    const seconds = now.getSeconds()
     const secondsDeg = ((seconds / 60) * 360) + 90;
-    secondHand.css('transform', `rotate(${secondsDeg}deg)`)
 
-    const mins = now.getMinutes();
+    // if at the end, change transition to prevent the hand from rewinding to the beginning.
+
+    if (secondsDeg === 444) {
+        secondHand.css('transition', 'none')
+        secondHand.css('transform', `rotate(${secondsDeg}deg)`)
+    } else {
+        secondHand.css('transition', 'all 0.05s')
+        secondHand.css('transform', `rotate(${secondsDeg}deg)`)
+    }
+
+    //_________________ MINUTES
+
+    const mins = now.getMinutes()
     const minsDeg = ((mins / 60) * 360) + 90;
-    minHand.css('transform', `rotate(${minsDeg}deg)`);
+    if (minsDeg === 444) {
+        minHand.css('transition', 'none')
+        minHand.css('transform', `rotate(${minsDeg}deg)`)
+    } else {
+        minHand.css('transition', 'all 0.05s')
+        minHand.css('transform', `rotate(${minsDeg}deg)`)
+    }
 
-    let hours = now.getHours();
-    if (hours > 12) hours -= 12
-    const hoursDeg = ((hours / 12) * 360) + 90;
-    hourHand.css('transform', `rotate(${hoursDeg}deg)`)
+    //_________________ HOURS    
+
+    let hours = now.getHours()
+    if (hours > 12) {
+        hours -= 12;
+    }
+    const hoursDeg = ((hours / 12) * 360) + 90
+
+    if (hoursDeg === 420) {
+        hourHand.css('transition', 'none')
+        hourHand.css('transform', `rotate(${hoursDeg}deg)`)
+    } else {
+        hourHand.css('transition', 'all 0.05s')
+        hourHand.css('transform', `rotate(${hoursDeg}deg)`)
+    }
+
 
 }
+
 
 // Start Clock
 setInterval(setDate, 1000)
