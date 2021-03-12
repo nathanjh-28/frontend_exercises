00337a100f22c6d14c180af85ead8a41339c2653
nathanjh-28 <nathan@nathanjharris.com>
2021-03-12 11:33:25 -0700

    minute hand working

--- ap/css_clock/script.js
+++ ap/css_clock/script.js
@@ -1,9 +1,14 @@
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    console.log(seconds);
+    //console.log(seconds);
     const secondsDeg = ((seconds / 60) * 360) + 90;
     $('.second-hand').css('transform', `rotate(${secondsDeg}deg)`)
+
+    const mins = now.getMinutes();
+    const minsDeg = ((mins / 60) * 360) + 90;
+    $('.min-hand').css('transform', `rotate(${minsDeg}deg)`);
+
 }
 
 
