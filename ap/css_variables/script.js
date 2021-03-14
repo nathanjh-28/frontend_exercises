1b2893b3fc3e27da91306838cb797fad3cff0e90
nathanjh-28 <nathan@nathanjharris.com>
2021-03-13 20:17:56 -0700

    css variables project complete

--- ap/css_variables/script.js
+++ ap/css_variables/script.js
@@ -1,1 +1,10 @@
-console.log('starter project engaged')
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
