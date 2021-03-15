332223642f1b1a4cc47ccdf61388ccabdfdb4d3e
nathanjh-28 <nathan@nathanjharris.com>
2021-03-14 21:56:09 -0600

    relaxer app complete

--- ap/relaxer-app/script.js
+++ ap/relaxer-app/script.js
@@ -1,1 +1,22 @@
-console.log('starter project engaged')
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2 // 3s
+const holdTime = totalTime / 5; // 1.5s
+
+function breathAnimation() {
+    text.innerText = "Breathe In!"
+    container.className = 'container grow'
+    setTimeout(() => {
+        text.innerText = "Hold!"
+        setTimeout(() => {
+            text.innerText = "Breathe Out!"
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breatheTime)
+
+}
+
+setInterval(breathAnimation, totalTime)
+
