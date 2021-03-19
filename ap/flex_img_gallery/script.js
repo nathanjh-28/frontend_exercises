f1994e8acf7b622b244f631958cd3f5cb67aafee
nathanjh-28 <nathan@nathanjharris.com>
2021-03-18 22:13:17 -0600

    flex img gallery complete

--- ap/flex_img_gallery/script.js
+++ ap/flex_img_gallery/script.js
@@ -1,1 +1,16 @@
-console.log('starter project engaged')
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+    }
+
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
